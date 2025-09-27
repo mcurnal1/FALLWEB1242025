fb97a5b992ad55e378f34a0d9be35047481e67ae
mcurnal1 <mcurnal1@stumail.jccc.edu>
2025-09-27 14:19:10 -0500

    Update project2.js

--- project2/project2.js
+++ project2/project2.js
@@ -1,8 +1,9 @@
 var myName=" Maryam Curnal";
-
-
  para1 = document.getElementById("p1");
-
-
-
 para1.textContent = myName;
+
+var n1 = 47;
+var n2 = 47;
+
+n1 + n2 = numberSum;
+document.getElementById("p3").textContent = numberMult;
